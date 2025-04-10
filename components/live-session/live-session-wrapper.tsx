929833a639744ff9dac8278cf800c77ca01c428c
rorez <pintabot.member@gmail.com>
2025-04-10 19:07:33 +0900

    fix: Adjust height calculation for chat component layout

--- components/live-session/live-session-wrapper.tsx
+++ components/live-session/live-session-wrapper.tsx
@@ -255,7 +255,7 @@ export function LiveSessionWrapper({ liveSessionData, isCreator }: LiveSessionWr
             />
           </div>
           <div className="md:h-screen md:sticky md:top-0">
-            <div className="h-[calc(100vh-569px)] md:h-[570px] px-3 mt-1">
+            <div className="h-[calc(100vh-495px)] md:h-[570px] px-3 mt-1">
               {/* Pass channel to ChatComponent */}
               {chatChannel && (
                 <ChatComponent
